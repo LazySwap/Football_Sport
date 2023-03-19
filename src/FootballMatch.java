0815b311d51af9fedf74e16e4a73ab034126e565
dvaibhavim <swapnilp692@gmail.com>
2023-03-19 20:06:20 +0100

    Added score board and summary code

---
+++ src/FootballMatch.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class FootballMatch {
+    public String homeTeam;
+    public String awayTeam;
+    public int homeScore;
+    public int awayScore;
+
+    public FootballMatch(String home, String away) {
+        homeTeam = home;
+        awayTeam = away;
+        homeScore = 0;
+        awayScore = 0;
+    }
+}
+
+	
---
+++ src/Sportradar.java
@@ -0,0 +1,29 @@
+
+public class Sportradar {
+
+	public static void main(String[] args) {
+	    LiveFootballScoreboard scoreboard = new LiveFootballScoreboard();
+
+	    scoreboard.startNewMatch("Mexico", "Canada");
+	    scoreboard.updateScore(0, 5);
+	    scoreboard.finishMatch();
+
+	    scoreboard.startNewMatch("Spain", "Brazil");
+	    scoreboard.updateScore(10, 2);
+	    scoreboard.finishMatch();
+
+	    scoreboard.startNewMatch("Germany", "France");
+	    scoreboard.updateScore(2, 2);
+	    scoreboard.finishMatch();
+
+	    scoreboard.startNewMatch("Uruguay", "Italy");
+	    scoreboard.updateScore(6, 6);
+	    scoreboard.finishMatch();
+
+	    scoreboard.startNewMatch("Argentina", "Australia");
+	    scoreboard.updateScore(3, 1);
+	    scoreboard.finishMatch();
+
+	    scoreboard.getSummary();
+	}
+}
